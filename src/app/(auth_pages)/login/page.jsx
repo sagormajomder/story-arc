4277e01cd85a25b06284e74c7a4fd9b2a0853493
Sagor Majomder <sagormajomder@gmail.com>
2026-01-14 23:22:09 +0600

    add loading state to login page with Suspense component

--- src/app/(auth_pages)/login/page.jsx
+++ src/app/(auth_pages)/login/page.jsx
@@ -1,5 +1,6 @@
 import AuthLeftSection from '@/components/auth/AuthLeftSection';
 import LoginForm from '@/components/auth/LoginForm';
+import { Suspense } from 'react';
 
 export default function LoginPage() {
   return (
@@ -8,7 +9,14 @@ export default function LoginPage() {
         title='Your next chapter starts here.'
         subtitle='Join thousands of readers tracking their journeys and discovering their next favorite story.'
       />
-      <LoginForm />
+      <Suspense
+        fallback={
+          <div className='flex justify-center items-center w-full lg:w-1/2'>
+            Loading...
+          </div>
+        }>
+        <LoginForm />
+      </Suspense>
     </main>
   );
 }
